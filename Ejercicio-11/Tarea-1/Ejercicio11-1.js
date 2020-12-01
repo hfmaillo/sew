4bac97752c4f853fadc0326f372e32d9b5321af6
uo271913 <uo271913@uniovi.es>
2020-12-01 17:08:31 +0100

    p3

---
+++ Ejercicio-11/Tarea-1/Ejercicio11-1.js
@@ -0,0 +1,48 @@
+// Ejercicio11.js
+// Ejercicio 11
+// Version 1.0. 17/11/2020. Hector Fernandez Maillo. Universidad de Oviedo
+"use strict";
+class GeoLocalizacion { 
+
+    constructor () {
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this));
+    }
+
+    getPosicion(posicion) {
+        this.longitud         = posicion.coords.longitude; 
+        this.latitud          = posicion.coords.latitude;  
+        this.precision        = posicion.coords.accuracy;
+        this.altitud          = posicion.coords.altitude;
+        this.precisionAltitud = posicion.coords.altitudeAccuracy;
+        this.rumbo            = posicion.coords.heading;
+        this.velocidad        = posicion.coords.speed;       
+    }
+
+    getLongitud() {
+        return this.longitud;
+    }
+
+    getLatitud() {
+        return this.latitud;
+    }
+
+    getAltitud() {
+        return this.altitud;
+    }
+
+    verTodo(dondeVerlo) {
+        var ubicacion = document.getElementById(dondeVerlo);
+        var datos = '<ul>'; 
+        datos += '<li>Longitud: '+this.longitud +' grados</li>'; 
+        datos += '<li>Latitud: '+this.latitud +' grados</li>';
+        datos += '<li>Precisión de la latitud y longitud: '+ this.precision +' metros</li>';
+        datos += '<li>Altitud: '+ this.altitude +' metros</li>';
+        datos += '<li>Precisión de la altitud: '+ this.precisionAltitud +' metros</li>'; 
+        datos += '<li>Rumbo: '+ this.rumbo +' grados</li>'; 
+        datos += '<li>Velocidad: '+ this.velocidad +' metros/segundo</li>';
+        datos += '</ul>';
+        ubicacion.innerHTML = datos;
+    }
+
+}
+var geo = new GeoLocalizacion();
