eaa04c5d152c4a64c349fab46fcc3d255eda308f
uo271913 <uo271913@uniovi.es>
2020-12-06 23:21:53 +0100

    p4

--- Ejercicio-3/CalculadoraRPN.php
+++ Ejercicio-3/CalculadoraRPN.php
@@ -5,30 +5,67 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Calculadora científica</title>
-    <link rel="stylesheet" href="CalculadoraCientifica.css" />
+    <title>Calculadora RPN</title>
+    <link rel="stylesheet" href="CalculadoraRPN.css" />
 </head>
 
 <body>
-    <!-- Calculadora Científica. Versión 1.0. 01/12/2020. Héctor Fernández Maillo. Universidad de Oviedo -->
+    <!-- Calculadora RPN. Versión 1.0. 01/12/2020. Héctor Fernández Maillo. Universidad de Oviedo -->
     <header>
-        <h1>Calculadora científica</h1>
+        <h1>Calculadora RPN</h1>
     </header>
 
     <article>
-        <h2 hidden>Calculadora científica</h2>
+        <h2 hidden>Calculadora RPN</h2>
 
         <?php
             // Iniciar SESSION
             session_start();
 
             /**
-             * Definición de la clase CalculadoraBasica
+             * Definición de la clase PilaLIFO
              */
-            class CalculadoraBasica {
+            class PilaLIFO {
+
+                protected $pila;
+
+                public function __construct() { 
+                    $this->pila = array();
+                }
+            
+                public function apilar($valor) {
+                    array_unshift($this->pila, $valor);
+                }
+            
+                public function desapilar() {
+                    return array_shift($this->pila);
+                }
+            
+                public function longitud() {
+                    return count($this->pila);
+                }
+            
+                public function mostrar() {
+                    $stringPila = "<h3 hidden>Pila</h3>";
+                    
+                    for ($i = 0; $i < $this->longitud(); $i++) {
+                        if ($i == 0) $stringPila .= "<ol>";
+                        $stringPila .= "<li>"  .$this->pila[$i]. "</li>";
+                        if ($i == $this->longitud() - 1) $stringPila .= "</ol>";
+                    }
+
+                    return $stringPila;
+                }
+            
+            }
+
+            /**
+             * Definición de la clase CalculadoraRPN
+             */
+            class CalculadoraRPN {
 
                 protected $pantalla;
-                protected $memoria;
+                protected $pila;
 
                 public function __construct() {
                     if (!isset($_SESSION['pantalla'])) {
@@ -36,17 +73,21 @@
                     }
                     $this->pantalla = $_SESSION['pantalla'];
 
-                    if (!isset($_SESSION['memoria'])) {
-                        $_SESSION['memoria'] = 0.0;
+                    if (!isset($_SESSION['pila'])) {
+                        $_SESSION['pila'] = new PilaLIFO();
                     }
-                    $this->memoria = $_SESSION['memoria'];
+                    $this->pila = $_SESSION['pila'];
                 }
 
                 public function getPantalla() {
                     return $this->pantalla;
                 }
 
-                protected function appendPantalla($toAppend) {
+                public function getPila() {
+                    return $this->pila;
+                }
+
+                private function appendPantalla($toAppend) {
                     $_SESSION['pantalla'] = $this->pantalla .= $toAppend;
                 }
 
@@ -58,148 +99,123 @@
                     $this->appendPantalla(".");
                 }
 
+                private function operacion($operador, $numOperandos) {
+                    if ($this->pila->longitud() >= $numOperandos) {
+                        $operacion;
+                        if ($numOperandos == 1) {
+                            $v = $this->pila->desapilar();
+                            $operacion = $operador($v);
+                        } else { // $numOperandos == 2
+                            $v2 = $this->pila->desapilar();
+                            $v1 = $this->pila->desapilar();
+                            $operacion = $operador($v1, $v2);
+                        }
+            
+                        $this->pila->apilar($operacion);
+                    }
+                }
+
                 public function suma() {
-                    $this->appendPantalla("+");
+                    $this->operacion(fn($v1, $v2) => $v1 + $v2, 2);
                 }
 
                 public function resta() {
-                    $this->appendPantalla("-");
+                    $this->operacion(fn($v1, $v2) => $v1 - $v2, 2);
                 }
 
                 public function multiplicacion() {
-                    $this->appendPantalla("*");
+                    $this->operacion(fn($v1, $v2) => $v1 * $v2, 2);
                 }
 
                 public function division() {
-                    $this->appendPantalla("/");
-                }
-                
-                protected function writePantalla($toWrite) {
-                    $_SESSION['pantalla'] = $this->pantalla = $toWrite;
-                }
-
-                public function mrc() {
-                    $this->writePantalla($this->memoria);
-                }
-
-                public function mMenos() {
-                    $this->igual();
-                    $valor = floatval($this->pantalla);
-                    if ($valor != 0) {
-                        $_SESSION['memoria'] = $this->memoria -= $valor;
-                    }
-                }
-
-                public function mMas() {
-                    $this->igual();
-                    $valor = floatval($this->pantalla);
-                    if ($valor != 0) {
-                        $_SESSION['memoria'] = $this->memoria += $valor;
-                    }
+                    $this->operacion(fn($v1, $v2) => $v1 / $v2, 2);
                 }
 
                 public function borrar() {
-                    $this->writePantalla("");
+                    $_SESSION['pantalla'] = $this->pantalla = "";
                 }
 
-                public function igual() {
-                    try {
-                        $this->pantalla = eval("return $this->pantalla ;");
-                    } catch (Throwable $e) {
-                        $this->pantalla = "Error: " .$e->getMessage();
+                public function enter() {
+                    $valor = floatval($this->pantalla);
+                    if ($valor != 0) {
+                        $this->borrar();
+                        $this->pila->apilar($valor);
+                    }
+                }
+
+                public function pi() {
+                    $this->appendPantalla(pi());
+                }
+
+                public function e() {
+                    $this->appendPantalla(exp(1));
+                }
+
+                public function seno() {
+                    $this->operacion('sin', 1);
+                }
+
+                public function coseno() {
+                    $this->operacion('cos', 1);
+                }
+
+                public function tangente() {
+                    $this->operacion('tan', 1);
+                }
+
+                public function arcoseno() {
+                    $this->operacion('asin', 1);
+                }
+
+                public function arcocoseno() {
+                    $this->operacion('acos', 1);
+                }
+
+                public function arcotangente() {
+                    $this->operacion('atan', 1);
+                }
+
+                public function raiz() {
+                    $this->operacion('sqrt', 1);
+                }
+
+                public function potencia() {
+                    $this->operacion('pow', 2);
+                }
+
+                public function logaritmo() {
+                    $this->operacion('log10', 1);
+                }
+
+                public function lNatural() {
+                    $this->operacion('log', 1);
+                }
+
+                public function masMenos() {
+                    if (substr($this->pantalla, 0, 1) != "-") {
+                        $this->pantalla = "-" .$this->pantalla;
+                    } else {
+                        $this->pantalla = substr($this->pantalla, 1);
                     }
                     $_SESSION['pantalla'] = $this->pantalla;
                 }
 
             }
 
-            /**
-             * Definición de la clase CalculadoraCientifica
-             */
-            class CalculadoraCientifica extends CalculadoraBasica {
-
-                public function pi() {
-                    $this->appendPantalla("pi()");
-                }
-
-                public function e() {
-                    $this->appendPantalla("exp(1)");
-                }
-
-                public function seno() {
-                    $this->appendPantalla("sin(");
-                }
-
-                public function coseno() {
-                    $this->appendPantalla("cos(");
-                }
-
-                public function tangente() {
-                    $this->appendPantalla("tan(");
-                }
-
-                public function pIzquierdo() {
-                    $this->appendPantalla("(");
-                }
-
-                public function pDerecho() {
-                    $this->appendPantalla(")");
-                }
-
-                public function arcoseno() {
-                    $this->appendPantalla("asin(");
-                }
-
-                public function arcocoseno() {
-                    $this->appendPantalla("acos(");
-                }
-
-                public function arcotangente() {
-                    $this->appendPantalla("atan(");
-                }
-
-                public function raiz() {
-                    $this->appendPantalla("sqrt(");
-                }
-
-                public function potencia() {
-                    $this->appendPantalla("pow(");
-                }
-
-                public function logaritmo() {
-                    $this->appendPantalla("log10(");
-                }
-
-                public function lNatural() {
-                    $this->appendPantalla("log(");
-                }
-
-                public function coma() {
-                    $this->appendPantalla(",");
-                }
-
-            }
-
-            $calculadora = new CalculadoraCientifica();
+            $calculadora = new CalculadoraRPN();
 
             // Solo se ejecutará si se ha pulsado un botón
             if (count($_POST) > 0) {
                 if(isset($_POST['pi'])) $calculadora->pi();
-                if(isset($_POST['e'])) $calculadora->e();
                 if(isset($_POST['seno'])) $calculadora->seno();
                 if(isset($_POST['coseno'])) $calculadora->coseno();
                 if(isset($_POST['tangente'])) $calculadora->tangente();
+                if(isset($_POST['e'])) $calculadora->e();
 
-                if(isset($_POST['pIzquierdo'])) $calculadora->pIzquierdo();
-                if(isset($_POST['pDerecho'])) $calculadora->pDerecho();
+                if(isset($_POST['raiz'])) $calculadora->raiz();
                 if(isset($_POST['arcoseno'])) $calculadora->arcoseno();
                 if(isset($_POST['arcocoseno'])) $calculadora->arcocoseno();
                 if(isset($_POST['arcotangente'])) $calculadora->arcotangente();
-
-                if(isset($_POST['raiz'])) $calculadora->raiz();
-                if(isset($_POST['mrc'])) $calculadora->mrc();
-                if(isset($_POST['mMenos'])) $calculadora->mMenos();
-                if(isset($_POST['mMas'])) $calculadora->mMas();
                 if(isset($_POST['division'])) $calculadora->division();
 
                 if(isset($_POST['potencia'])) $calculadora->potencia();
@@ -220,36 +236,35 @@
                 if(isset($_POST['digitos3'])) $calculadora->digitos(3);
                 if(isset($_POST['suma'])) $calculadora->suma();
 
-                if(isset($_POST['coma'])) $calculadora->coma();
+                if(isset($_POST['masMenos'])) $calculadora->masMenos();
                 if(isset($_POST['digitos0'])) $calculadora->digitos(0);
                 if(isset($_POST['punto'])) $calculadora->punto();
                 if(isset($_POST['borrar'])) $calculadora->borrar();
-                if(isset($_POST['igual'])) $calculadora->igual();
+                if(isset($_POST['enter'])) $calculadora->enter();
             }
 
             $pantalla = $calculadora->getPantalla();
+            $pila = $calculadora->getPila()->mostrar();
 
             // Interfaz con el usuario. En el interior de comillas dobles se deben usar comillas simples
             echo "
                 <form action='#' method='post' name='calculadora'>
+                    <section id='pila'>
+                        $pila
+                    </section>
+
                     <input type='text' title='Pantalla' value='$pantalla' disabled />
 
                     <input type='submit' name='pi' value='π' />
-                    <input type='submit' name='e' value='e' />
                     <input type='submit' name='seno' value='sin' />
                     <input type='submit' name='coseno' value='cos' />
                     <input type='submit' name='tangente' value='tan' />
+                    <input type='submit' name='e' value='e' />
 
-                    <input type='submit' name='pIzquierdo' value='(' />
-                    <input type='submit' name='pDerecho' value=')' />
+                    <input type='submit' name='raiz' value='√' />
                     <input type='submit' name='arcoseno' value='asin' />
                     <input type='submit' name='arcocoseno' value='acos' />
                     <input type='submit' name='arcotangente' value='atan' />
-
-                    <input type='submit' name='raiz' value='√' />
-                    <input type='submit' name='mrc' value='mrc' />
-                    <input type='submit' name='mMenos' value='m-' />
-                    <input type='submit' name='mMas' value='m+' />
                     <input type='submit' name='division' value='/' />
 
                     <input type='submit' name='potencia' value='x^y' />
@@ -270,11 +285,11 @@
                     <input type='submit' class='digito' name='digitos3' value='3' />
                     <input type='submit' name='suma' value='+' />
 
-                    <input type='submit' name='coma' value=',' />
+                    <input type='submit' name='masMenos' value='±' />
                     <input type='submit' class='digito' name='digitos0' value='0' />
                     <input type='submit' name='punto' value='.' />
                     <input type='submit' name='borrar' value='C' />
-                    <input type='submit' name='igual' value='=' />
+                    <input type='submit' name='enter' value='Enter' />
                 </form>
             ";
         ?>
